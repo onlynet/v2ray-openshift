0bb11ef2f669218f2ec53e78606e6748557624ec
onlynet <boyhc118@qq.com>
2021-04-24 17:33:09 +0800

    modify script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
-sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" /etc/v2ray/config.json
-sed -i "s/CLIENT_SECURITY/$CLIENT_SECURITY/g" /etc/v2ray/config.json
-v2ray -config=/etc/v2ray/config.json &
+sed -i "s/CLIENT_ID/$CLIENT_ID/g" conf/config.json
+sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" conf/config.json
+sed -i "s/CLIENT_SECURITY/$CLIENT_SECURITY/g" conf/config.json
+v2ray -config=conf/config.json &
 nginx -g 'daemon off;'
